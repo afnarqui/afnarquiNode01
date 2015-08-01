8d0a46e9ce8b938b635abf554ebedc2002ed64b2
Andrés felipe naranjo quintero <afnarqui@hotmail.com>
2015-08-01 05:07:13 +0200

    se implementa angular y foundation
    se implementa angular con css por medio de fundation

---
+++ public/controller.js
@@ -0,0 +1,28 @@
+angular.module('app',[])
+.controller('appController',function ($scope,$http){
+
+	$scope.post = [];
+	$scope.Nuevopost = {};
+
+	$http.get('http://jsonplaceholder.typicode.com/posts')
+		.success(function(data){
+			$scope.post = data;
+		})
+		.error(function(err){
+			console.log(err);
+		});
+
+	$scope.Crearpost = function(){
+		$http.post('http://jsonplaceholder.typicode.com/posts',{
+			title : $scope.Nuevopost.title,
+			body : $scope.Nuevopost.body,
+			userId: 1
+		})
+			.success(function(data,status,headers,conf){
+				$scope.post.push(data);
+			})
+			.error(function(err,status,headers,conf){
+				console.log(err)
+			});
+	}
+});
